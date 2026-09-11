93f937a8f35236fb279f7b7de3bc263d18ec73a8
saiganeshakula93-debug <saiganeshakula93@gmail.com>
2026-09-11 08:31:07 +0530

    Revert "Fix PORT parsing by launching via python main.py"
    This reverts commit 2e23ee6445b2e4b90d3f3fa4ad9703be6ba3d8fa.

--- main.py
+++ main.py
@@ -3676,8 +3676,3 @@ FRONTEND_HTML = r"""<!DOCTYPE html>
 </body>
 </html>
 """
-
-if __name__ == "__main__":
-    import uvicorn
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
